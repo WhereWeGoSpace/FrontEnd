f6e3aee20c3ce9b2a8aa180d3d195c1dccbdebc4
QiFenPocket <QiFen.Pocket@gmail.com>
2017-09-30 02:33:46 +0800

    1. Change get to ajax 2. Handle credit card verify failed 3. Date from UTC to local time

--- src/js/logic.js
+++ src/js/logic.js
@@ -1,28 +1,65 @@
 
 $(function () {
     $("#click").click(function () {
-        $.get("http://localhost:3000/travelInfo", function (data) {
-            $("#startSite").text(data[0].startSite);
-            $("#endSite").text(data[0].endSite);
-            $("#date").text(data[0].date);
-            $("#price").text(data[0].price);
+        $.ajax({
+            url: 'http://localhost:3000/travelInfo',
+            type: 'GET',
+            data: {
+            },
+            error: function(xhr) {
+              alert('Ajax request 發生錯誤');
+            },
+            success: function(data) {
+                let date = new Date(data[0].timestamp);
 
-            $("#bar").slideUp("slow", function () {
-                $("#result").fadeIn();
-            });
+                $("#startSite").text(data[0].startSite);
+                $("#endSite").text(data[0].endSite);
+                $("#date").text(date.toLocaleDateString("en-US"));
+                $("#price").text(data[0].price);
+    
+                $("#bar").slideUp("slow", function () {
+                    $("#result").fadeIn();
+                });
+            }
         });
     });
 
     $("#checkout").click(function () {
         $("#result").slideUp("slow", function () {
-            $("#checkout  ").fadeIn();
+            $("#creditCardVerifyInfo").fadeIn();
         });
     });
 
     $("#pay").click(function () {
-        var that = this;
-        $.get("http://localhost:3000/ticketDownloadLink", function (data) {
-            window.open(data.url, "_blank");
+        $.ajax({
+            url: 'http://localhost:3000/ticketDownloadLink',
+            type: 'GET',
+            data: {
+                cardNumber: $("#cardNumber").val(),
+                expirationDate: $("#expirationDate").val(),
+                verifyNumber: $("#verifyNumber").val()
+            },
+            error: function(xhr) {
+              alert('Ajax request 發生錯誤');
+            },
+            success: function(data) {
+                if (!data.success)
+                {
+                    $("#creditCardVerifyInfo").slideUp("slow", function () {
+                        $("#creditCardVerifyError").fadeIn();
+                    });
+                }
+                else
+                {
+                    window.open(data.url, "_blank");
+                }
+            }
+        });
+    });
+    
+    $("#backToCreditCardInfoInput").click(function () {
+        $("#creditCardVerifyError").slideUp("slow", function () {
+            $("#creditCardVerifyInfo").fadeIn();
         });
     });
 });

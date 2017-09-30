d403c342210abe0134bbbc0344efa1d531ae13a9
Jonathan <xingulin@gmail.com>
2017-09-30 03:00:33 +0000

    Add unit testing mechanism to build.bat

--- build.bat
+++ build.bat
@@ -7,6 +7,12 @@ rem start testing http server
 cd src
 start cmd /k http-server -p 8080
 
+rem execute unit testing
+cd ../test
+qunit-cli testGeneratePaymentInfo.js
+qunit-cli testGetTravelInfo.js
+cd ..
+
 rem execute end to end testing
 cd ../test
 cmd /c .\node_modules\.bin\cucumber.js.cmd

9e78e38552903ce8f003b11c0933744cdf051868
diro <diro.fan@gmail.com>
2017-09-30 16:16:27 +0800

    Add test case for generatePaymentInfo(credit card

--- test/testGeneratePaymentInfo.js
+++ test/testGeneratePaymentInfo.js
@@ -1,24 +1,37 @@
 if (typeof QUnit == 'undefined') // if your tests also run in the browser...
 {
-	QUnit = require('qunit-cli');
+    QUnit = require('qunit-cli');
 }
 
 if (typeof module !== 'undefined' && module.exports) {
-	var logic = require('../src/js/logic.js')
-	generateBookingInfo = logic.generateBookingInfo;	
+    var logic = require('../src/js/logic.js')
+    generateBookingInfo = logic.generateBookingInfo;
+    generatePaymentInfo = logic.generatePaymentInfo;
 }
 
-QUnit.test("test_credit_card_payment_info", function (assert) {
-    var paymentInfo = generateBookingInfo("1", "2", "3", "4", "5", {"from": "qwe", "to": "rit"});
+QUnit.test("test_generate_BookingInfo", function (assert) {
+    var bookingInfo = generateBookingInfo("1", "2", "3", "4", "5", { "from": "qwe", "to": "rit" });
 
-    assert.ok("qwe" == paymentInfo.From_Code, "Passed!");
-    assert.ok("rit" == paymentInfo.To_Code, "Passed!");
-    assert.ok("12" == paymentInfo.Contactor.name, "Passed!");
-    assert.ok("3" == paymentInfo.Contactor.email, "Passed!");
-    assert.ok("1" == paymentInfo.Passengers[0].first_name, "Passed!");
-    assert.ok("2" == paymentInfo.Passengers[0].last_name, "Passed!");
-    assert.ok("4" == paymentInfo.Passengers[0].birthdate, "Passed!");
-    assert.ok("3" == paymentInfo.Passengers[0].email, "Passed!");
-    assert.ok("5" == paymentInfo.Passengers[0].passport, "Passed!");
+    assert.ok("qwe" == bookingInfo.From_Code, "Passed!");
+    assert.ok("rit" == bookingInfo.To_Code, "Passed!");
+    assert.ok("12" == bookingInfo.Contactor.name, "Passed!");
+    assert.ok("3" == bookingInfo.Contactor.email, "Passed!");
+    assert.ok("1" == bookingInfo.Passengers[0].first_name, "Passed!");
+    assert.ok("2" == bookingInfo.Passengers[0].last_name, "Passed!");
+    assert.ok("4" == bookingInfo.Passengers[0].birthdate, "Passed!");
+    assert.ok("3" == bookingInfo.Passengers[0].email, "Passed!");
+    assert.ok("5" == bookingInfo.Passengers[0].passport, "Passed!");
 
 });
+
+QUnit.test("test_credit_card_payment_info", function (assert) {
+    var paymentInfo = generatePaymentInfo(1, 2, 3, 4, { "bookingID": "123456" });
+
+    assert.ok("123456" == paymentInfo.BookingId, "Passed!");
+    assert.ok("1" == paymentInfo.CreditCard.number, "Passed!");
+    assert.ok("2" == paymentInfo.CreditCard.exp_year, "Passed!");
+    assert.ok("3" == paymentInfo.CreditCard.exp_month, "Passed!");
+    assert.ok("4" == paymentInfo.CreditCard.cvv, "Passed!");
+
+});
+

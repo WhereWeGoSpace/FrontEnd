4fcdf07d2d4cceb786f40f1f32aa160ba0a52263
diro <diro.fan@gmail.com>
2017-09-30 13:10:57 +0800

    Rename the Mock Object

--- test/testGetTravelInfo.js
+++ test/testGetTravelInfo.js
@@ -5,25 +5,25 @@ if (typeof QUnit == 'undefined') // if your tests also run in the browser...
 
 if (typeof module !== 'undefined' && module.exports) {
 	var logic = require('../src/js/logic.js')
-	processTravelInfo = logic.processTravelInfo;	
+	processTravelInfo = logic.processTravelInfo;
 }
 
 QUnit.test("test_travel_info", function (assert) {
 	let travelInfo = [{ "timestamp": 1506732963000, "startSite": "Taipei", "endSite": "Taoyuan", "price": "100" }]
 
-	var MockText = function () { };
+	var MockHTMLElement = function () { };
 
-	var startSite = new MockText();
-	var endSite = new MockText();
-	var date = new MockText();
-	var price = new MockText();
-	var bar = new MockText();
+	var startSite = new MockHTMLElement();
+	var endSite = new MockHTMLElement();
+	var date = new MockHTMLElement();
+	var price = new MockHTMLElement();
+	var bar = new MockHTMLElement();
 
 	startSite.text = function (data) { this.text = data };
 	endSite.text = function (data) { this.text = data };
 	date.text = function (data) { this.text = data };
 	price.text = function (data) { this.text = data };
-	bar.slideUp = function (a, b) { };
+	bar.slideUp = function (a, b) { bar.slideUpIsCalled = true };
 
 	processTravelInfo(travelInfo, startSite, endSite, date, price, bar)
 
@@ -31,5 +31,6 @@ QUnit.test("test_travel_info", function (assert) {
 	assert.ok(endSite.text == "Taoyuan", "Passed!");
 	assert.ok(date.text == "9/30/2017", "Passed!");
 	assert.ok(price.text == "100", "Passed!");
+	assert.ok(bar.slideUpIsCalled == true, "Passed!");
 
 });
